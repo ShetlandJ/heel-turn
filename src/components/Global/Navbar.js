00ff227caecf456927831cb785ba6e9a11c83195
James <james@jastewart.co.uk>
2020-10-24 15:18:27 +0100

    Adding a tailwind config file for breakpoints + mapping the links

--- src/components/Global/Navbar.js
+++ src/components/Global/Navbar.js
@@ -5,6 +5,26 @@ import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
+
+  const linkData = [
+    { to: '/', text: 'Home' },
+    { to: '/about', text: 'About' },
+    { to: '/test', text: 'Test' },
+  ];
+
+  const links = linkData.map((link) => (
+    <NavLink
+      activeClassName="active"
+      to={link.to}
+      isActive={(match, location) => location.pathname === link.to}
+      className="mb-2 lg:mb-0"
+    >
+      <span className="lg:ml-2">
+        {link.text}
+      </span>
+    </NavLink>
+  ));
+
   return (
     <>
       <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-black mb-3">
@@ -25,15 +45,16 @@ export default function Navbar() {
           </div>
           <div
             className={
-              `lg:flex flex-grow items-center${
+              `lg:flex flex-grow ${
                 navbarOpen ? ' flex' : ' hidden'}`
             }
           >
-            <ul className="flex text-white flex-col lg:flex-row list-none lg:ml-auto">
-              <NavLink
+            <div className="flex w-full text-white flex-col lg:flex-row list-none lg:ml-auto lg:justify-end">
+              {/* <NavLink
                 activeClassName="active"
                 to="/"
                 isActive={(match, location) => location.pathname === '/'}
+                className="mb-2 lg:mb-0"
 
               >
                 Home
@@ -42,19 +63,22 @@ export default function Navbar() {
                 activeClassName="active"
                 to="/about"
                 isActive={(match, location) => location.pathname === '/about'}
+                className="mb-2 lg:mb-0"
               >
-                <span className="ml-2">About</span>
+                <span className="lg:ml-2">About</span>
 
               </NavLink>
               <NavLink
                 activeClassName="active"
                 to="/test"
                 isActive={(match, location) => location.pathname === '/test'}
+                className="mb-2 lg:mb-0"
               >
-                <span className="ml-2">Test</span>
+                <span className="lg:ml-2">Test</span>
 
-              </NavLink>
-            </ul>
+              </NavLink> */}
+              {links}
+            </div>
           </div>
         </div>
       </nav>
